36fb7987c126dc039f20bf42ac80ef112a940d06
Karczel <92095164+Karczel@users.noreply.github.com>
2024-09-17 04:08:54 +0700

    Exercise 1

--- test_statistics.py
+++ test_statistics.py
@@ -1,11 +1,37 @@
 """Unit test for statistics.py functions."""
 from unittest import TestCase
-from statistics import variance, stdev
+from statistics import average, variance, stdev
 from math import sqrt
 
 
 class StatisticsTest(TestCase):
-    """Unit test of variance and stdev function."""
+    """Unit test of average, variance and stdev function."""
+    def test_average_typical_values(self):
+        """Test average with typical values."""
+        self.assertEqual(average([1, 2, 3, 4, 5]), 3.0)  # Typical case
+        self.assertEqual(average([10.0, 20.0, 30.0]), 20.0)  # All floats
+        self.assertEqual(average([1, 1, 1]), 1.0)  # All same values
+
+    def test_average_empty(self):
+        """Test average with an empty list."""
+        with self.assertRaises(ValueError):
+            average([])  # Assuming default average for empty list is 0.0
+
+    def test_average_single_value(self):
+        """Test average with a single value."""
+        self.assertEqual(average([5]), 5.0)  # Single value
+
+    def test_average_large_numbers(self):
+        """Test average with very large numbers."""
+        self.assertEqual(average([1e6, 1e6 + 1, 1e6 - 1]), 1e6)  # Large numbers
+
+    def test_average_negative_numbers(self):
+        """Test average with negative numbers."""
+        self.assertEqual(average([-1, -2, -3, -4]), -2.5)  # Negative values
+
+    def test_average_mixed_numbers(self):
+        """Test average with mixed int and float numbers."""
+        self.assertEqual(average([1, 2.5, 3, 4.75]), 2.8125)  # Mixed int and float
 
     def test_variance_typical_values(self):
         """Variance of typical values."""
@@ -13,12 +39,56 @@ class StatisticsTest(TestCase):
         self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
         self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
+    def test_variance_decimal_values(self):
+        """Test variance with decimal values."""
+        self.assertAlmostEqual(variance([0.1, 4.1]), 4.0)
+        self.assertAlmostEqual(variance([0.1, 4.1, 4.1, 8.1]), 8.0)
+
+    def test_variance_single_element(self):
+        """Test variance with a single element."""
+        self.assertEqual(variance([5]), 0.0)
+
     def test_variance_non_integers(self):
         """Variance should work with decimal values."""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
         self.assertAlmostEqual(4.0, variance([0.1, 4.1]))
         # variance([0,4,4,8]) == 8
         self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
+
+    def test_variance_empty_list(self):
+        """Variance of an empty list should raise an error."""
+        with self.assertRaises(ValueError):
+            variance([])
+
+    def test_variance_single_value(self):
+        """Variance of a single value should be zero."""
+        self.assertEqual(0.0, variance([10.0]))
+
+    def test_variance_large_values(self):
+        """Variance of very large numbers."""
+        self.assertAlmostEqual(2.6666666666666668e+16, variance([1e8, 1e8 + 2e8, 1e8 - 2e8]))
+
+    def test_variance_negative_numbers(self):
+        """Test variance with negative numbers."""
+        self.assertAlmostEqual(variance([-10, -2, -8, -4, -6]), 8.0)
+
+    def test_variance_identical_values(self):
+        """Test variance with identical values."""
+        self.assertEqual(variance([42, 42, 42, 42]), 0.0)
+
+    def test_variance_floats(self):
+        """Test variance with floating-point numbers."""
+        self.assertAlmostEqual(variance([1.5, 2.0, 2.5]), 0.16666666666666666)
+
+    def test_variance_large_dataset(self):
+        """Test variance with a large dataset."""
+        large_data = [i for i in range(1000)]
+        expected_variance = sum((x - 499.5) ** 2 for x in large_data) / len(large_data)
+        self.assertAlmostEqual(variance(large_data), expected_variance)
+
+    def test_variance_integers_and_floats(self):
+        """Test variance with a mix of integers and floats."""
+        self.assertAlmostEqual(variance([1, 2.5, 3]), 0.7222222222222222)
 
     def test_stdev(self):
         """Standard deviation of typical values."""
@@ -29,8 +99,34 @@ class StatisticsTest(TestCase):
         # variance([0, 0.5, 1, 1.5, 2.0]) is 0.5
         self.assertEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]))
 
+    def test_stdev_empty_list(self):
+        """Standard deviation of an empty list should raise an error."""
+        with self.assertRaises(ValueError):
+            stdev([])
 
-if __name__ == '__main__':
-    import unittest
+    def test_stdev_single_element(self):
+        """Test standard deviation with a single element."""
+        self.assertEqual(0.0, stdev([10]))
 
-    unittest.main(verbosity=1)
+    def test_stdev_negative_numbers(self):
+        """Test standard deviation with negative numbers."""
+        self.assertAlmostEqual(2.8284271247461903, stdev([-10, -2, -8, -4, -6]))
+
+    def test_stdev_large_numbers(self):
+        """Test standard deviation with very large numbers."""
+        self.assertAlmostEqual(sqrt(2.6666666666666668e16), stdev([1e8, 1e8 + 2e8, 1e8 - 2e8]))
+
+    def test_stdev_identical_values(self):
+        """Test standard deviation with identical values."""
+        self.assertEqual(0.0, stdev([99, 99, 99, 99]))
+
+    def test_stdev_floats(self):
+        """Test standard deviation with floating-point numbers."""
+        self.assertAlmostEqual(sqrt(variance([1.5, 2.0, 2.5])), stdev([1.5, 2.0, 2.5]))
+
+
+
+# if __name__ == '__main__':
+#     import unittest
+#
+#     unittest.main(verbosity=1)
